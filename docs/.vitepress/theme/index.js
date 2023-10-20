0a76f9105b7eda25243b5a2f819d91fd11ecea07
wzyll <3145801375@qq.com>
2023-10-21 02:16:35 +0800

    🐞 fix(删除index.css 引用):

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -3,7 +3,7 @@ import "./tailwind.css";
 import "./var.css";
 import "./article.css";
 import "./print.css";
-import "./index.css"; /* 设计html 相关标签的全局样式 */
+
 
 import LinkCard from "../components/LinkCard.vue";
 import WarnBlock from "../components/warnBlock.vue";

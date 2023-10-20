78e9ca5a2c0b517611652bc039640abc18ea74dd
wzyll <3145801375@qq.com>
2023-10-21 02:12:54 +0800

    ✨ feat(🎉添加自定义组件warnBlog):

---
+++ docs/.vitepress/components/warnBlock.vue
@@ -0,0 +1,16 @@
+
+/* 用来定义一个表示注意的引用 */
+<template>
+  <span class="custom-block">
+    <slot></slot>
+  </span>
+</template>
+
+<style scoped>
+.custom-block {
+  background-color: #facbd299; 
+  color: black; 
+  padding: 2px 4px;
+  border-radius: 3px;
+}
+</style>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -3,12 +3,16 @@ import "./tailwind.css";
 import "./var.css";
 import "./article.css";
 import "./print.css";
+import "./index.css"; /* 设计html 相关标签的全局样式 */
 
 import LinkCard from "../components/LinkCard.vue";
+import WarnBlock from "../components/warnBlock.vue";
 
 export default {
   extends: DefaultTheme,
   enhanceApp(ctx) {
     ctx.app.component("LinkCard", LinkCard);
+    ctx.app.component("WarnBlock", WarnBlock);
+ 
   },
 };

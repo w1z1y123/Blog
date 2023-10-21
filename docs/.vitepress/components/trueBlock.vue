b1436477b16452edbfa926d50b9040f9556276f3
wzyll <3145801375@qq.com>
2023-10-21 17:23:22 +0800

    ✨ feat:
    添加新组件trueBlock

---
+++ docs/.vitepress/components/trueBlock.vue
@@ -0,0 +1,16 @@
+
+/* 用来定义一个表示注意的引用 */
+<template>
+  <span class="custom-block">
+    <slot></slot>
+  </span>
+</template>
+
+<style scoped>
+.custom-block {
+  background-color: #b7f1b399; 
+  color: rgb(62, 156, 89);
+  padding: 1px 1px;
+  border-radius: 3px;
+}
+</style>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -7,12 +7,14 @@ import "./print.css";
 
 import LinkCard from "../components/LinkCard.vue";
 import WarnBlock from "../components/warnBlock.vue";
+import trueBlock from "../components/trueBlock.vue";
 
 export default {
   extends: DefaultTheme,
   enhanceApp(ctx) {
     ctx.app.component("LinkCard", LinkCard);
     ctx.app.component("WarnBlock", WarnBlock);
+    ctx.app.component("trueBlock", trueBlock);
  
   },
 };
